4329cfe850e22f4a81dcc42b9f034ae06bb21599
Zev Blut <zblut@cerego.co.jp>
2010-08-20 01:16:23 +0900

    Update tracking to file. Ignore log files.

--- rubykaigi.el
+++ rubykaigi.el
@@ -283,14 +283,19 @@
 ;; post-command-hook
 
 
+(setq rubykaigidribble "rubykaigidribble.txt")
+(create-file-buffer rubykaigidribble)
+;; for now user will have to save dribble
 
 (defun track-command-executed ()
   "Records command executed and keys pressed"
-    (message "cmd: %s keys %s\n" this-command (this-command-keys)))
+  (with-current-buffer rubykaigidribble
+    (insert (format "cmd: %s keys %s\n" this-command (this-command-keys)))))
 
 (defun track-mode-change ()
   "Record the major mode and all minor-modes activated"
-  (message "major-mode: %s\nminor-modes: %s\n\n" major-mode minor-mode-list))
+  (with-current-buffer rubykaigidribble
+    (insert (format "major-mode: %s\nminor-modes: %s\n\n" major-mode minor-mode-list))))
 
 (progn
   (add-hook 'pre-command-hook 'track-command-executed)
